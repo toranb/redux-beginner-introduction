d0d0030dd3b65711be731d6ef4708591753f6695
toran billups <toranb@gmail.com>
2016-01-08 20:01:05 -0600

    step 6: added custom provider/passed store as context

--- index.js
+++ index.js
@@ -11,37 +11,57 @@ const reducer = ((state=0, action) => {
 
 class Basement extends React.Component {
     render() {
-        const store = this.props.store;
+        const store = this.context.store;
         const state = store.getState();
         return (
             <div>basement ... {state}</div>
         );
     }
 }
+Basement.contextTypes = {
+    store: React.PropTypes.object
+}
 class MiddleOne extends React.Component {
     render() {
-        const store = this.props.store;
         return (
-            <div>middle and <Basement store={store}/></div>
+            <div>middle and <Basement /></div>
         );
     }
 }
 class TopLevel extends React.Component {
     componentDidMount() {
-        const store = this.props.store;
+        const store = this.context.store;
         store.subscribe(() => {
             this.forceUpdate();
         });
     }
     render() {
-        const store = this.props.store;
+        const store = this.context.store;
         const state = store.getState();
         return (
-            <div>{state} <MiddleOne store={store} /><button onClick={() => { store.dispatch({type: 'UP'}); }}>up</button></div>
+            <div>{state} <MiddleOne /><button onClick={() => { store.dispatch({type: 'UP'}); }}>up</button></div>
         );
     }
 }
+TopLevel.contextTypes = {
+    store: React.PropTypes.object
+}
+class Provider extends React.Component {
+    getChildContext() {
+        return {
+            store: this.props.store
+        }
+    }
+    render() {
+        return this.props.children;
+    }
+}
+Provider.childContextTypes = {
+    store: React.PropTypes.object
+}
 
 render((
-    <TopLevel store={createStore(reducer)}/>
+    <Provider store={createStore(reducer)}>
+      <TopLevel />
+    </Provider>
 ), document.getElementById('container'));
