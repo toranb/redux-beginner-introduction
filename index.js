c2f2adfcf98eeb41ff821323313340e002c20e3d
toran billups <toranb@gmail.com>
2016-01-08 19:49:15 -0600

    step 1: redux basics

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('start here');
+import { createStore } from 'redux';
+
+const reducer = ((state=0, action) => {
+    if(action.type === 'UP') {
+        return state + 1;
+    }
+    return state;
+});
+
+//step 1: create it
+const store = createStore(reducer);
+
+//step 2: get the state in the store
+const render = () => {
+    document.body.innerText = store.getState();
+};
+
+//step 3: change the state in the store
+document.addEventListener('click', () => {
+    store.dispatch({type: 'UP'});
+});
+
+//step 4: subscribe to changes and re-render the state
+store.subscribe(render);
+
+render();
