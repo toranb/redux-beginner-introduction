6773907e13e23b93d42756a37c3388b8849edb6c
toran billups <toranb@gmail.com>
2016-01-08 19:54:33 -0600

    step 3: store no longer a global variable

--- index.js
+++ index.js
@@ -9,19 +9,18 @@ const reducer = ((state=0, action) => {
     return state;
 });
 
-//step 1: create it
-const store = createStore(reducer);
-
 //step 2: get the state in the store (with render)
 //step 3: change the state in the store (with onClick)
 //step 4: subscribe to changes and forceUpdate (re-render)
 class TopLevel extends React.Component {
     componentDidMount() {
+        const store = this.props.store;
         store.subscribe(() => {
             this.forceUpdate();
         });
     }
     render() {
+        const store = this.props.store;
         const state = store.getState();
         return (
             <div>{state} <button onClick={() => { store.dispatch({type: 'UP'}); }}>up</button></div>
@@ -29,6 +28,7 @@ class TopLevel extends React.Component {
     }
 }
 
+//step 1: create it as a prop and pass it down
 render((
-    <TopLevel />
+    <TopLevel store={createStore(reducer)}/>
 ), document.getElementById('container'));
